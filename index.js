87da17fcb4b407961edf4719e6a20b02336ce384
Milo Wroblewski <m@MacBook-Pro-di-Milo.local>
2018-10-24 16:37:44 +0200

    hello response

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ restService.post("/echo", function(req, res) {
       : "Seems like some problem. Speak again.";
   return res.json({
     speech: 'hello',
-    displayText: speech,
+    displayText: 'hello',
     source: "webhook-echo-sample"
   });
 });
